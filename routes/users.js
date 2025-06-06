8ccc3f5ef233e6f595383689f8ab5f3b3190121d
jeanpaullopes <jean.lopes@ulife.com.br>
2025-06-06 20:52:57 -0300

    commit final

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,35 @@
 import express from 'express';
 import firebaseServices from '../services/firebaseServices.js';
+import { JWTSecret } from '../services/firebaseCredeiciais.js';
+import { SignJWT, jwtVerify } from 'jose';
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let token = req.get( 'Authorization' );
+  if (!token) {
+    res.status(401).send('Não autorizado, token não informado');
+    return;
+  }
+  token = token.split(' ')[1];
+  if (!token) {
+    res.status(498).send('Não autorizado, token inválido');
+    return;
+  }
+  jwtVerify(token, JWTSecret, { algorithms: ['HS256'] })
+  .then((payload) =>{
+    res.send(payload);
+  })
+  .catch((error) => {
+    console.log(error);
+    if (error.code == 'ERR_JWT_EXPIRED') {
+      res.status(498).send("Token expirado " + error.code);
+    } else {
+      res.status(498).send("Token inválido " + error.code);
+    }
+    return;
+  });
+  
 });
 
 router.post('/login', function(req, res, next) {
@@ -13,7 +38,31 @@ router.post('/login', function(req, res, next) {
     req.body.senha)
     .then((user) => {
       console.log(user);
-      res.status(200).send(user);
+    let payload={
+      user: 'Sei lá quem é',
+      email: user.email,
+      nivelAcesso: 'Pica das galaxias'
+    };
+
+    new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setSubject('Login Way dia '+new Date())
+    .setJti('123')
+    .setExpirationTime('60s')
+    .sign(JWTSecret)
+    .then( (token) => {
+      res.status(200).send(token);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  
+
+
+
+
+      
     })
     .catch((error) => {
       console.log(error);
--- services/firebaseCredeiciais.js
+++ services/firebaseCredeiciais.js
@@ -1,4 +1,6 @@
 // Your web app's Firebase configuration
+import { createSecretKey } from 'crypto';
+
 export const firebaseConfig = {
   apiKey: "AIzaSyAUub_Ml-JhmdmmjVBOdEwECaGszNWU1FA",
   authDomain: "oincrivelprojetodapaeladaapi.firebaseapp.com",
@@ -7,3 +9,7 @@ export const firebaseConfig = {
   messagingSenderId: "812274976282",
   appId: "1:812274976282:web:a40ce7ee47fe9b433873a3"
 };
+
+const secret = "nosso segredinho"
+export let JWTSecret = createSecretKey(secret, 'utf-8')
+
